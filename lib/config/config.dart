1e96d24ac6b2a2a4a62583639eb96f7be52244d9
KOTARO <kou0704can@gmail.com>
2023-11-13 22:58:27 +0900

    Create file for Google Auth

---
+++ lib/config/config.dart
@@ -0,0 +1,15 @@
+ class Configurations {
+      static const _apiKey = "AIzaSyCdLHsr6X-Ob0Ps_azBGsykADqqJPcvP9s";
+      static const _authDomain = "mybookshelf-890c5.firebaseapp.com";
+      static const _projectId = "mybookshelf-890c5";
+      static const _storageBucket = "mybookshelf-890c5.appspot.com";
+      static const _messagingSenderId ="688526243352";
+      static const _appId = "1:688526243352:web:2de46872d71c322ad4356d";
+    
+      String get apiKey => _apiKey;
+      String get authDomain => _authDomain;
+      String get projectId => _projectId;
+      String get storageBucket => _storageBucket;
+      String get messagingSenderId => _messagingSenderId;
+      String get appId => _appId;
+    }
---
+++ lib/utils/auth.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+// Google ログイン関数
+Future<void> signInWithGoogle() async {
+  try {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) return;
+
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+    // ログイン成功時の処理
+    print("success");
+
+    // ログイン成功後、ユーザー情報を取得する例
+    // final User user = userCredential.user!;
+    // print("User ID: ${user.uid}");
+    // print("Display Name: ${user.displayName}");
+    // print("Email: ${user.email}");
+  } catch (e) {
+    print("Google ログインエラー: $e");
+  }
+}
