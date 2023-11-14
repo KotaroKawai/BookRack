7227bc71cca6256ae675d8c0607bf41027529de3
KOTARO <kou0704can@gmail.com>
2023-11-14 20:08:02 +0900

    Add FireBase for Web

---
+++ lib/config/config.dart
@@ -0,0 +1,15 @@
+ class Configurations {
+      static const _apiKey = "AIzaSyCnAKLfzlvnUa6zJJxITMGwzROFIIsIYgY";
+      static const _authDomain = "bookshelf-f4247.firebaseapp.com";
+      static const _projectId = "bookshelf-f4247";
+      static const _storageBucket = "bookshelf-f4247.appspot.com";
+      static const _messagingSenderId ="407594844473";
+      static const _appId = "1:407594844473:web:41c033a1903a826a68e8e8";
+    
+      String get apiKey => _apiKey;
+      String get authDomain => _authDomain;
+      String get projectId => _projectId;
+      String get storageBucket => _storageBucket;
+      String get messagingSenderId => _messagingSenderId;
+      String get appId => _appId;
+    }
