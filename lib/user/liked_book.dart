0924c3a6a5028d9d8fd484b4c55759282bbfe0d7
Marley Mulvin Broome <marley.developer@gmail.com>
2023-11-15 19:13:22 +0900

    Userクラスを実装,Userの気に入りの本の管理と取得

---
+++ lib/user/liked_book.dart
@@ -0,0 +1,13 @@
+class LikedBook {
+  final String title;
+  final String authors;
+  final String imageUrl;
+  final String id;
+
+  LikedBook({
+    required this.title,
+    required this.authors,
+    required this.imageUrl,
+    required this.id,
+  });
+}
